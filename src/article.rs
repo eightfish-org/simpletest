ccd017b0395049267bfb144834f25fb16ca5f72a
Daogang Tang <daogangtang@gmail.com>
2025-05-08 10:32:43 +0800

    upgrade to new sdk and spin_worker.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/article.rs
+++ src/article.rs
@@ -1,13 +1,15 @@
 use anyhow::{anyhow, bail};
 use eightfish_derive::EightFishModel;
-use eightfish_sdk::{
-    EightFishModel, HandlerCRUD, Info, Module, Request, Response, Result, Router, Status,
-};
+use eightfish_sdk::{EightFishModel, Module, Request, Response, Result, Router, Status};
 use serde::{Deserialize, Serialize};
-use spin_sdk::pg::{self, DbValue, Decode, ParameterValue};
+use spin_sdk::pg::{DbValue, Decode, ParameterValue};
+use spin_worker::{
+    sql_create_one, sql_delete, sql_delete_one, sql_query, sql_query_one, sql_update,
+    sql_update_one,
+};
 
-const REDIS_URL_ENV: &str = "REDIS_URL_ENV";
-const DB_URL_ENV: &str = "DB_URL_ENV";
+const REDIS_URL: &str = "REDIS_URL";
+const DB_URL: &str = "DB_URL";
 
 #[derive(Debug, Clone, Serialize, Deserialize, EightFishModel, Default)]
 pub struct Article {
@@ -20,12 +22,12 @@ pub struct Article {
 pub struct ArticleModule;
 
 impl ArticleModule {
-    fn get_one(req: &mut Request) -> Result<Response<Article>> {
+    fn get_one(req: &mut Request) -> Result<Response> {
         let params = req.parse_urlencoded()?;
         let article_id = params.get("id").ok_or(anyhow!("id error"))?;
 
         let article = sql_query_one!(Article, article_id);
-        let results = if let Some(article) = article {
+        let results: Vec<Article> = if let Some(article) = article {
             vec![article]
         } else {
             bail!("no this item".to_string());
@@ -34,7 +36,7 @@ impl ArticleModule {
         Ok(Response::new(Status::Successful, results))
     }
 
-    fn new_article(req: &mut Request) -> Result<Response<Article>> {
+    fn new_article(req: &mut Request) -> Result<Response> {
         let params = req.parse_urlencoded()?;
         let title = params
             .get("title")
@@ -61,15 +63,19 @@ impl ArticleModule {
             authorname,
         };
 
-        if let Err(_) = sql_create_one!(article) {
-            // handle error
-        }
-        let ret = vec![article];
+        // let ret = sql_query!(Article, "", &[]);
+        // let ret = sql_delete!(req, Article, "", &[]);
+        // let ret = sql_update!(req, Article, "", &[]);
+        let ret = sql_create_one!(req, article);
 
-        Ok(Response::new(Status::Successful, ret))
+        if let Ok(article) = ret {
+            Ok(Response::new(Status::Successful, vec![article]))
+        } else {
+            Ok(Response::from_str(Status::Failed, "error".to_string()))
+        }
     }
 
-    fn update(req: &mut Request) -> Result<Response<Article>> {
+    fn update(req: &mut Request) -> Result<Response> {
         let params = req.parse_urlencoded()?;
 
         let id = params.get("id").ok_or(anyhow!("id error"))?.to_owned();
@@ -86,7 +92,7 @@ impl ArticleModule {
             .ok_or(anyhow!("authorname error"))?
             .to_owned();
 
-        let old_article = sql_query_one!(Article, id);
+        let old_article = sql_query_one!(Article, &id);
         match old_article {
             Some(old_article) => {
                 let article = Article {
@@ -97,12 +103,12 @@ impl ArticleModule {
                     ..old_article
                 };
 
-                if let Err(_) = sql_update_one!(article) {
-                    // handle error
+                if let Ok(instance) = sql_update_one!(req, article) {
+                    let ret = vec![instance];
+                    Ok(Response::new(Status::Successful, ret))
+                } else {
+                    bail!("update action: db operation error.")
                 }
-                let ret = vec![article];
-
-                Ok(Response::new(Status::Successful, ret))
             }
             None => {
                 bail!("update action: no item in db")
@@ -110,32 +116,22 @@ impl ArticleModule {
         }
     }
 
-    fn delete(req: &mut Request) -> Result<Response<Article>> {
+    fn delete(req: &mut Request) -> Result<Response> {
         let params = req.parse_urlencoded()?;
 
         let id = params.get("id").ok_or(anyhow!("id error"))?.to_owned();
-        
+
         // ensure there is the target item in db
-        let article: Article = sql_query_one!(Article, id);
-
-        if let Err(_) = sql_delete_one!(article) {
-            // handle error
+        if let Some(article) = sql_query_one!(Article, &id) {
+            if let Ok(instance) = sql_delete_one!(req, article) {
+                let ret = vec![instance];
+                Ok(Response::new(Status::Successful, ret))
+            } else {
+                bail!("delete action: error in db")
+            }
+        } else {
+            bail!("delete action: no item in db")
         }
-        let ret = vec![article];
-
-        Ok(Response::new(Status::Successful, ret))
-
-        // let (sql, sql_params) = Article::build_delete(id.as_str());
-        // _ = pg_conn.execute(&sql, &sql_params)?;
-
-        // let info = Info {
-        //     model_name: Article::model_name(),
-        //     action: HandlerCRUD::Delete,
-        //     extra: "".to_string(),
-        // };
-        // let results: Vec<Article> = vec![];
-
-        // Ok(Response::new(Status::Successful, info, results))
     }
 
     fn version(_req: &mut Request) -> Result<Response> {
@@ -150,8 +146,8 @@ impl Module for ArticleModule {
     fn router(&self, router: &mut Router) -> Result<()> {
         router.get("/simpletest/v1/article", Self::get_one);
         router.post("/simpletest/v1/article/new", Self::new_article);
-        router.post("/simpletest/v1/article/update", Self::update);
-        router.post("/simpletest/v1/article/delete", Self::delete);
+        router.put("/simpletest/v1/article/update", Self::update);
+        router.delete("/simpletest/v1/article/delete", Self::delete);
 
         router.get("/simpletest/v1/version", Self::version);
 
