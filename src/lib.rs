4042485d23d09beb9d315cdc5bcd46782af2df8c
Daogang Tang <daogangtang@gmail.com>
2025-05-22 20:35:35 +0800

    use OnceLock to increase performace.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 use anyhow::Result;
 use bytes::Bytes;
 use spin_sdk::redis_component;
+use std::sync::OnceLock;
+// use std::cell::OnceCell;
 
 use eightfish_sdk::{App as EightFishApp, GlobalFilter, Request, Response, Result as EFResult};
 
@@ -27,14 +29,21 @@ pub fn build_app() -> EightFishApp {
     sapp
 }
 
-// OnceCell
+// Static Lazy to hold the Worker instance
+static WORKER: OnceLock<spin_worker::Worker> = OnceLock::new();
 
 /// Main entry
 #[redis_component]
 fn on_message(message: Bytes) -> Result<()> {
-    // later put this construtor to outer OnceCell
-    let app = build_app();
-    let sw = spin_worker::Worker::mount(app);
+    let sw = WORKER.get_or_init(|| {
+        let app = build_app();
+        let mut sw = spin_worker::Worker::mount(app);
+        sw.set_on_block_height(|_height, _hash| {
+            // println!("block height: {height}");
+            // println!("block hash: {hash}");
+        });
+        sw
+    });
 
     sw.work(message)?;
 
